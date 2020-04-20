ac7d8f2ba0c6cd617d5414f9627e1831d7779294
William Aponte <apontewilliam16@gmail.com>
2020-04-20 08:21:28 -0400

    second commit

---
+++ Functions.py
@@ -0,0 +1,26 @@
+
+def User_Input(inputs):
+#asking for user input 
+    inputs.input1 = input("Please Give me Number to add:")
+    int =(inputs.input1)
+    inputs.input2 = input("Please Give me Number to add:")
+    int =(inputs.input2)
+    
+    
+
+def addition(num1,num2):
+    #Implementation will go here
+    return (num1 + num2)
+    pass 
+
+
+def results(result):
+    print("This is your result:")
+    
+def name_addition_result():
+    
+    pass
+
+
+User_Input(inputs)
+name_addition_result()
